dbdef291f5d4665b7785bc326fe5f85f9f2e9094
Rahil Mehta <65954612+Karma-Yogi-Rahil@users.noreply.github.com>
2024-04-28 18:42:29 -0700

    Update Folder structure

---
+++ homework-1-main/tictactoe/test/GameView.java
@@ -0,0 +1,40 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class GameView {
+
+
+    private JFrame frame;
+    private JButton[][] buttons ;
+    private JTextArea playerDisplay;
+
+
+    private GameView(){
+        buttons = new JButton[3][3];
+        frame = new JFrame("Tic Tac Toe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(300, 300));
+        frame.setLayout(new BorderLayout());
+
+        JPanel boardPanel = new JPanel(new GridLayout(3, 3));
+        frame.add(boardPanel, BorderLayout.CENTER);
+
+        playerDisplay = new JTextArea();
+        frame.add(playerDisplay, BorderLayout.SOUTH);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j] = new JButton();
+                buttons[i][j].setPreferredSize(new Dimension(100, 100));
+                boardPanel.add(buttons[i][j]);
+            }
+        }
+
+        frame.setVisible(true);
+
+    }
+
+
+}
